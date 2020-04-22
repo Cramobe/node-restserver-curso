39057a4b38ff2d2feccf6009f19ae94b525cdfd7
Cramobe Meitres <cristian.mosquera@meitres.com.co>
2020-04-22 16:50:49 -0500

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 require('./config/config')
 
 const express = require('express');
+const mongoose = require('mongoose');
+
+
 const app = express();
 
 
@@ -12,40 +15,21 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use( require('./routes/usuario'));
 
-app.get('/usuario', (req, res) =>{
-    res.json('get usuario')
+
+
+
+mongoose.connect(process.env.URLDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+},(err,res)=>{
+    if(err) throw err;
+    console.log('Base de datos conectada');
 });
-
-app.post('/usuario', (req, res) =>{
-
-    let body = req.body;
-
-    if( body.nombre === undefined){
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    }else{
-        res.json({
-            persona: body
-        });
-    }
-
-});
-
-app.put('/usuario/:id', (req, res) =>{
-
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', (req, res) =>{
-    res.json('delete usuario')
-});
+// .then(()=> console.log('base de datos ONLINE'))
+// .catch(err => console.log('No se pudo conectar', err));
 
 app.listen(process.env.PORT, () => {
     console.log(`Escuchando en el puerto ${process.env.PORT}`);
